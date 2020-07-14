d2e386667ac1c4180d0dd8335ffdb93c9fdd2361
kogepan1209 <k.hosono1209@gmail.com>
2020-07-14 22:45:29 +0900

    New

--- 20200714/ex02.py
+++ 20200714/ex02.py
@@ -1,13 +1,20 @@
 # ex02
 
 def count_items(itemlist):
-   if itemlist == []:
-       return []
-   if not isinstance(itemlist, list):
-       return len(itemlist)
-   return len(itemlist[0]) + len(itemlist[1:])
+    if len(itemlist) == 0:
+        return 0
+    if not isinstance(itemlist, list):
+        return 1
+    return count_items(itemlist[0]) + count_items(itemlist[1:])
 
 lst1 = ["a", "b", "c"]
+print 'count_items(', lst1, ') =', count_items(lst1)
+
 lst2 = ["a", ["b", "c"], "d"]
-print(f"count_items{lst1} = {count_items(lst1)}")
-print(f"count_items{lst2} = {count_items(lst2)}")
+print 'count_items(', lst2, ') =', count_items(lst2)
+
+lst3 = ["a", ["b", ["c", "d", "e"], ["f", "g"]]]
+print 'count_items(', lst3, ') =', count_items(lst3)
+
+str1 = "abc"
+print 'count_items(', str1, ') =', count_items(str1)
--- 20200714/ex03.py
+++ 20200714/ex03.py
@@ -1,21 +1,33 @@
 #ex03
-
-# リストを逆順にする関数
 def rev(lst):
-   if lst == []:
-          return []
-   else:
-          return rev(lst[1:]) + [lst[0]]
+    if lst == []:
+        return []
+    else:
+        return rev(lst[1:]) + [lst[0]]
 
 def deeprev(lst):
-   if lst == []:
-       return []
-   else:
-       return deeprev(lst[1:]) + [lst[0]]
+    if lst == []:
+        return []
+    if isinstance(lst[0], list):
+        lst[0] = deeprev(lst[0])
+    return deeprev(lst[1:]) + [lst[0]]
 
+def count_items(itemlist):
+    if len(itemlist) == 0:
+        return 0
+    if not isinstance(itemlist, list):
+        return 1
+    return len(itemlist[0]) + count_items(itemlist[1:])
 
 # main
-lst = [0, 1, 2]
-print(f"rev({lst}) = {rev(lst)}")
-lst1 = [[0, 1], [2, 3], [4, 5]]
-print(f"deeprev({lst1}) = {deeprev(lst1)}")
+lst1 = [0, 1, 2]
+print 'deeprev(', lst1, ') =', deeprev(lst1)
+
+lst2 = [[0, 1], [2, 3], [4, 5]]
+print 'deeprev(', lst2, ') =', deeprev(lst2)
+
+lst3 = [[[0, 1], [2, 3]], 4, [5, 6]]
+print 'deeprev(', lst3, ') =', deeprev(lst3)
+
+lst4 = [0, 1, [2, [3, 4]]]
+print 'deeprev(', lst4, ') =', deeprev(lst4)
